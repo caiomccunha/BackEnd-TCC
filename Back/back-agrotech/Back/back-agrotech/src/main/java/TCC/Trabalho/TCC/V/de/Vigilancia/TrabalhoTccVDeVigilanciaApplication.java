45a2aa4493e6203f73b900f6143be65a90f9f30d
Caio Messi <136396933+caiomccunha@users.noreply.github.com>
2025-07-03 21:28:43 -0300

    Arquivos de teste de usuários atualizados
    Testes de usuários funcionando 100%, mais pica que tudo

---
+++ Back/back-agrotech/Back/back-agrotech/src/main/java/TCC/Trabalho/TCC/V/de/Vigilancia/TrabalhoTccVDeVigilanciaApplication.java
@@ -0,0 +1,13 @@
+package TCC.Trabalho.TCC.V.de.Vigilancia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoTccVDeVigilanciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoTccVDeVigilanciaApplication.class, args);
+	}
+
+}
