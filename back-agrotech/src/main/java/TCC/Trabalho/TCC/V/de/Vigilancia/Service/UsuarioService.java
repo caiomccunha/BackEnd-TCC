a2bf88b2c65366f7f31baff798bb5cfd2b707dc5
Marcosware <marcosware.dev@gmail.com>
2025-08-13 07:06:26 -0300

    Service merge

--- back-agrotech/src/main/java/TCC/Trabalho/TCC/V/de/Vigilancia/Service/UsuarioService.java
+++ back-agrotech/src/main/java/TCC/Trabalho/TCC/V/de/Vigilancia/Service/UsuarioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
